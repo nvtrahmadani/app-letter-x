f73fcd2d9bba24aca53aaf7174270542cba299ea
Novita Rahmadani Sinaga <nvtrahmadani3@gmail.com>
2026-02-27 12:55:03 +0800

    feat_url_pdf_file4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Doctor;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('doctors/export-pdf', function () {
+
+    $doctors = Doctor::all();
+
+    $pdf = Pdf::loadView('pdf.doctors', [
+        'doctors' => $doctors
+    ])->setPaper('a4', 'landscape');
+
+    return $pdf->download('doctors.pdf');
+})->name('doctors.export-pdf');
